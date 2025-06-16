a91bac52a92b39e14c8fc43fe9da741a6aea31bd
kakashaUbivasha <makspala777@gmail.com>
2025-06-16 12:38:57 +0500

    projects model + user model update

--- database/migrations/2025_06_16_073632_create_projects_table.php
+++ database/migrations/2025_06_16_073632_create_projects_table.php
@@ -11,14 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->uuid('id')->primary();
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->enum('role', ['admin', 'user', 'manager'])->default('user');
-            $table->rememberToken();
+            $table->text('description');
+            $table->unsignedBigInteger('owner_id');
+            $table->foreign('owner_id')->references('id')->on('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
@@ -28,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('projects');
     }
 };
