e3e0b367686f154e936b289e998b9fe8dbb558b3
kakashaUbivasha <makspala777@gmail.com>
2025-06-19 18:10:59 +0500

    change column in table tasks

--- routes/api.php
+++ routes/api.php
@@ -15,10 +15,6 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
 Route::group(['middleware'=>'auth:api'], function (){
