699cefaceae6e1bb6b7ad78b3b448783dc9f6669
kakashaUbivasha <makspala777@gmail.com>
2025-06-20 22:23:49 +0500

    tag controller methods

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -2,9 +2,26 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\Tag\TagRequest;
+use App\Models\Tag;
 
 class TagController extends Controller
 {
-
+        public function index()
+        {
+            $tags = Tag::all();
+            return response()->json($tags);
+        }
+        public function store(TagRequest $request)
+        {
+            $data = $request->validated();
+            Tag::create($data);
+            return response()->json(['message'=>'Tag created'], 201);
+        }
+        public function destroy($id)
+        {
+            $tag = Tag::findOrFail($id);
+            $tag->delete();
+            return response()->json(['message'=>'Tag deleted'], 200);
+        }
 }
