f4e5eb83bd0739296f41ff93733f5a374b94e2c6
kakashaUbivasha <makspala777@gmail.com>
2025-06-23 19:17:26 +0500

    commend controller done

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -27,8 +27,22 @@ class CommentController extends Controller
         $comment = $commentService->createComment($user->id, $task->id, $data);
         return response()->json(['comments'=>$comment], 200);
     }
-    public function update()
+    public function update(CommentRequest $request, $project_id, $task_id, $comment_id, GlobalService $service, CommentService $commentService)
     {
-
+        $user = auth()->user();
+        $data = $request->validated();
+        $project = $service->checkProject($user->id, $project_id);
+        $task = $project->tasks()->findOrFail($task_id);
+        $comment = $task->comments()->findOrFail($comment_id);
+        $comment->update($data);
+        return response()->json(['comment'=>$comment], 200);
+    }
+    public function destroy($project_id, $task_id, $comment_id, GlobalService $service, CommentService $commentService){
+        $user = auth()->user();
+        $project = $service->checkProject($user->id, $project_id);
+        $task = $project->tasks()->findOrFail($task_id);
+        $comment = $task->comments()->findOrFail($comment_id);
+        $comment->delete();
+        return response()->json(['message'=>'Комментарий удалён'], 200);
     }
 }
